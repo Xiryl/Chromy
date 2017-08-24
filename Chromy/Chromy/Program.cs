9fd9a664d0fee464cda0c1fdc879abff0c65e475
Fabio Chiarani <fabio@chiarani.it>
2017-08-24 17:02:16 +0200

    [ADD] Support Core Files And Header

--- Chromy/Chromy/Program.cs
+++ Chromy/Chromy/Program.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Chromy
 {
@@ -14,13 +15,65 @@ namespace Chromy
     {
         static void Main(string[] args)
         {
-            #region Kill All Chrome Process
-            Process[] chromeInstances = Process.GetProcessesByName("chrome");
 
-            foreach (Process p in chromeInstances)
-                p.Kill();
+            #region Title
+            var sb = new StringBuilder();
+            var sbg = new StringBuilder();
+            var sbend = new StringBuilder();
 
+            sb.AppendLine(@"####################################################################################################");
+            sb.AppendLine("                                                                                                     ");
+            sb.AppendLine(@"               _..._                            .-'''-.                                             ");
+            sb.AppendLine(@"            .-'_..._''.                        '   _    \                                           ");
+            sb.AppendLine(@"          .' .'      '.\  .                  /   /` '.   \  __  __   ___                            ");
+            sb.AppendLine(@"         / .'           .'|                 .   |     \  ' |  |/  `.'   `..-.          .-           ");
+            sb.AppendLine(@"        . '            <  |         .-,.--. |   '      |  '|   .-.  .-.   '\ \\       / /           ");
+            sb.AppendLine(@"        | |             | |         |  .-. |\    \     / / |  |  |  |  |  | \ \      / /            ");
+            sb.AppendLine(@"        | |             | | .'''-.  | |  | | `.   ` ..' /  |  |  |  |  |  |  \ \    / /             ");
+            sb.AppendLine(@"        . '             | |/.'''. \ | |  | |    '-...-'`   |  |  |  |  |  |   \ \  / /              ");
+            sb.AppendLine(@"         \ '.          .|  /    | | | |  '-                |  |  |  |  |  |    \ `  /               ");
+            sb.AppendLine(@"          '. `._____.-'/| |     | | | |                    |__|  |__|  |__|     \  /                ");
+            sb.AppendLine(@"            `-.______ / | |     | | | |                                         / /                 ");
+            sb.AppendLine(@"                     `  | '.    | '.|_|                                     |`-' /                  ");
+            sb.AppendLine(@"                        '---'   '---'                                        '..'                   ");
+            sb.AppendLine("                                                                                                     ");
+            sbg.AppendLine(@"                                   _              _     ___        ");
+            sbg.AppendLine(@"                                  |_)_      _ .__|_  ||  | _  _ | _");
+            sbg.AppendLine(@"                                  | (_)\/\/(/_|  ||_|||  |(_)(_)|_>");
+            sbg.AppendLine(@"                                                                                                   ");
+            sbend.AppendLine(@"####################################################################################################");
+
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(sb);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(sbg);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(sbend);
             #endregion
+
+            Console.Write("                               Performing some task... ");
+            using (var progress = new ProgressBar())
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    progress.Report((double)i / 100);
+                    Thread.Sleep(20);
+                }
+            }
+            Console.WriteLine("Done.");
+
+            Console.ReadLine();
+
+
+            //#region Kill All Chrome Process
+            //var chromeInstances = Process.GetProcessesByName("chrome");
+
+            //foreach (Process p in chromeInstances)
+            //    p.Kill();
+
+            //#endregion
         }
     }
 }
