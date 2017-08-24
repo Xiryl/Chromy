1def51f63da5ab8cfe1b79a937ddadd4de2ba0a3
Fabio Chiarani <fabio@chiarani.it>
2017-08-24 16:12:45 +0200

    [ADD] Chrome Page Killer

--- Chromy/Chromy/Program.cs
+++ Chromy/Chromy/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,13 @@ namespace Chromy
     {
         static void Main(string[] args)
         {
-         
+            #region Kill All Chrome Process
+            Process[] chromeInstances = Process.GetProcessesByName("chrome");
+
+            foreach (Process p in chromeInstances)
+                p.Kill();
+
+            #endregion
         }
     }
 }
